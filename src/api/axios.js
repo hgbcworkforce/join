0dd2d768f7da9c48b88a4829dea4ecc83077686c
oluwaseyipd <oluwaseyipd@gmail.com>
2026-02-13 06:58:58 +0100

    fix: update API base URL in axios instance and Signin component

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  // baseURL: "https://firsttimer-u8aa.onrender.com/api",
 });
 
 // Automatically add the token to every request
